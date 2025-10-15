805fb1e7698acd4c7df64b61f56cbc21f9b4e5c6
Jai Desai <jai.desai.engr@gmail.com>
2025-10-15 23:09:33 +0530

    Create token_util.go

---
+++ Server/MagicStreamServer/utils/token_util.go
@@ -0,0 +1,101 @@
+package utils
+
+import (
+	"context"
+	"errors"
+	"os"
+	"time"
+
+	"StreamMovieServer/database"
+
+	"github.com/gin-gonic/gin"
+	jwt "github.com/golang-jwt/jwt/v5"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type SignedDetails struct {
+	Email     string
+	FirstName string
+	LastName  string
+	Userid    string
+	Role      string
+	jwt.RegisteredClaims
+}
+
+var SECRET_KEY string = os.Getenv("SECRET_KEY")
+var SECRET_REFRESH_KEY string = os.Getenv("SECRET_REFRESH_KEY")
+var userCollection *mongo.Collection = database.OpenCollection("users")
+
+func GenerateAllTokens(email string, firstName string, lastName string, userid string, role string) (string, string, error) {
+	claims := &SignedDetails{
+		Email:     email,
+		FirstName: firstName,
+		LastName:  lastName,
+		Role:      role,
+		Userid:    userid,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "StreamMovieServer",
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		return "", "", err
+	}
+
+	refreshClaims := &SignedDetails{
+		Email:     email,
+		FirstName: firstName,
+		LastName:  lastName,
+		Role:      role,
+		Userid:    userid,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "StreamMovieServer",
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * 7 * time.Hour)),
+		},
+	}
+	refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims)
+	signedRefreshToken, err := refreshToken.SignedString([]byte(SECRET_REFRESH_KEY))
+	if err != nil {
+		return "", "", err
+	}
+	return signedToken, signedRefreshToken, nil
+}
+
+func UpdateAllTokens(UserId, token, refreshToken string) error {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	updatedAt, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
+	updateData := bson.M{
+		"$set": bson.M{
+
+			"token":         token,
+			"refresh_token": refreshToken,
+			"updated_at":    updatedAt,
+		},
+	}
+	_, err := userCollection.UpdateOne(ctx, bson.M{"user_id": UserId}, updateData)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetAccessToken(c *gin.Context) string {
+	authHeader := c.Request.Header.Get("Authorization")
+	if authHeader == "" {
+		return "", errors.New("Authorization header is required")
+	}
+	tokenString := authHeader[len("Bearer "):]
+	if tokenString == "" {
+		return "", errors.New("Bearer token is required")
+	}
+
+	return tokenString
+}
