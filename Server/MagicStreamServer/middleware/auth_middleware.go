08b260c28624b18b1f13726bd40054465cc39c91
Jai Desai <jai.desai.engr@gmail.com>
2025-10-16 23:36:25 +0530

    Create auth_middleware.go

---
+++ Server/MagicStreamServer/middleware/auth_middleware.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"StreamMovieServer/utils"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token, err := utils.GetAccessToken(c)
+
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			c.Abort()
+			return
+		}
+		if token == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "No token is required"})
+			c.Abort()
+			return
+		}
+		claims, err := utils.ValidateToken(token)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+		c.Set("user_id", claims.Userid)
+		c.Set("role", claims.Role)
+
+		c.Next()
+	}
+}
